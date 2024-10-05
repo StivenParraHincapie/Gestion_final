0112305ca254bc5f0239abb3e5484d2ec60f6028
AndresRen <andresfereal@gmail.com>
2024-10-05 17:19:26 -0500

    Cursos,Grados,Estudiantes

---
+++ Gestion_Escuela/app/Student/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import Student
+from app.Courses.models import Course
+from app.Grades.models import Grade  
+
+class StudentSerializer(serializers.ModelSerializer):
+    cursos = serializers.PrimaryKeyRelatedField(many=True, queryset=Course.objects.all(), required=False)
+    calificaciones_estudiante = serializers.PrimaryKeyRelatedField(many=True, queryset=Grade.objects.all(), required=False)
+
+    class Meta:
+        model = Student
+        fields = ['id', 'nombre_completo', 'correo_electronico', 'numero_telefono', 
+                  'fecha_nacimiento', 'direccion', 'rol', 'nombre_usuario', 
+                  'contraseña', 'cursos', 'calificaciones_estudiante']  
+
+    def create(self, validated_data):
+        cursos = validated_data.pop('cursos', [])  
+        student = super().create(validated_data)  
+
+        for curso_id in cursos:
+            student.cursos.add(curso_id)  
+
+        return student
+
+    def update(self, instance, validated_data):
+        cursos = validated_data.pop('cursos', None)  
+        instance = super().update(instance, validated_data)
+
+        if cursos is not None:
+            instance.cursos.set(cursos)  
+
+        return instance
+    
+    
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'  
--- Gestion_Escuela/app/Student/views.py
+++ Gestion_Escuela/app/Student/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Student
+from .serializers import StudentSerializer, CourseSerializer  
+class StudentListCreateView(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
-# Create your views here.
+class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentCoursesView(APIView):
+    def get(self, request, pk):
+        try:
+           
+            student = Student.objects.get(id=pk)  
+            
+            
+            courses = student.cursos.all()  
+            
+            
+            serializer = CourseSerializer(courses, many=True)
+            return Response(serializer.data)
+        except Student.DoesNotExist:
+            
+            return Response({'error': 'Estudiante no encontrado'}, status=404)
