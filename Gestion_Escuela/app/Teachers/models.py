b2422dc1eff33050912eaa2fe23cacfdca2742aa
Stiven <stivenalexparra@gmail.com>
2024-10-05 17:13:15 -0500

    Teachers, Users Y Gestion escuela Cambios

--- Gestion_Escuela/app/Teachers/models.py
+++ Gestion_Escuela/app/Teachers/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from app.Users.models import Usuarios
 
-# Create your models here.
+class Teacher(Usuarios):  
+    departamento = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre_completo
--- Gestion_Escuela/app/Teachers/views.py
+++ Gestion_Escuela/app/Teachers/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Teacher
+from .serializers import TeacherSerializer
 
-# Create your views here.
+class TeacherListCreateView(generics.ListCreateAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class TeacherDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
